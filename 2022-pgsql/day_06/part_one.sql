ed99cd818d58a68f7346e35cfad95a1c678b7bb0
Martin Heralecký <heralecky.martin@gmail.com>
2022-12-06 22:09:25 +0100

    day 6

---
+++ 2022-pgsql/day_06/part_one.sql
@@ -0,0 +1,8 @@
+select i + s
+from
+    unnest(regexp_split_to_array(pg_read_file('/data/day_06/input.txt'), '')) with ordinality c(p, i),
+    generate_series(0, 3) s
+group by i + s
+having count(distinct p) = 4
+order by i + s
+limit 1;
